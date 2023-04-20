9629d1eed147f97488b8c4517b2d8c00c675ac4d
HyeSeong Jang <hyeseong7848@gmail.com>
2023-04-21 00:04:49 +0900

    기본 ExoPlayer 동작 추가

--- app/src/main/java/hs/project/comettube/MainActivity.kt
+++ app/src/main/java/hs/project/comettube/MainActivity.kt
@@ -1,8 +1,13 @@
 package hs.project.comettube
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.ui.PlayerControlView
+import com.google.android.exoplayer2.ui.PlayerView
 import hs.project.comettube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +16,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var videoAdapter: VideoAdapter
 
+    private var player: ExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -18,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         videoAdapter = VideoAdapter { videoItem ->
             binding.motionLayout.setTransition(R.id.collapse, R.id.expand)
             binding.motionLayout.transitionToEnd()
+            play(videoItem)
         }
 
         binding.motionLayout.jumpToState(R.id.collapse)
@@ -29,5 +37,41 @@ class MainActivity : AppCompatActivity() {
 
         val videoList = readData("video.json", VideoList::class.java) ?: VideoList(emptyList())
         videoAdapter.submitList(videoList.videos.toList())
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (player == null) {
+            initExoPlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (player == null) {
+            initExoPlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player?.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private fun initExoPlayer() {
+        player = ExoPlayer.Builder(this@MainActivity).build()
+            .also {
+                binding.playerView.player = it
+            }
+    }
+
+    private fun play(item: VideoItem) {
+        player?.setMediaItem(MediaItem.fromUri(Uri.parse(item.videoUrl)))
+        player?.prepare()
+        player?.play()
     }
 }
